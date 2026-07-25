52ada0782cad344e77a958d61a4652160b314c68
Prashik Meshram <meshrampa@rknec.edu>
2026-07-25 23:59:00 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+
+        return Arrays.equals(chars1, chars2);
+    }
+}
