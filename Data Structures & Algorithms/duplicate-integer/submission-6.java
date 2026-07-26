fbb971ea5e10925cae6464e8d8f9ebc3189836d6
Prashik Meshram <meshrampa@rknec.edu>
2026-07-26 17:58:07 +0530

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+            seen.add(nums[i]);
+        }
+        return false;
+    }
+}
